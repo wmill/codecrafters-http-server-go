5f84ad653e503ac4e2be0116fa895e658d4138a4
Walter Miller <waltermil@gmail.com>
2024-02-02 15:40:45 -0500

    stage 2

--- app/server.go
+++ app/server.go
@@ -15,10 +15,17 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		buffer := make([]byte, 1024)
+		conn.Read(buffer);
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	}
 }
+
